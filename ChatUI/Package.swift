0ee7958bd04b4985f700767e27527558793bf55c
Andres Raigoza <andresr774@gmail.com>
2026-03-13 15:33:06 -0500

    Add BubbleView

--- ChatUI/Package.swift
+++ ChatUI/Package.swift
@@ -1,23 +1,19 @@
 // swift-tools-version: 6.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "ChatUI",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "ChatUI",
-            targets: ["ChatUI"]
-        ),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "ChatUI"
-        ),
-
-    ]
+  name: "ChatUI",
+  platforms: [.iOS(.v26)],
+  products: [
+    .library(
+      name: "ChatUI",
+      targets: ["ChatUI"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "ChatUI"
+    ),
+  ]
 )
