c2a0e86ebef4b9ae47df1b8798e1bf1b41a43621
Andres Raigoza <andresr774@gmail.com>
2026-03-27 12:55:45 -0500

    Add Root and Chatlist Tests

---
+++ Chat/Tests/ChatTests/RootTests.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  Chat
+//
+//  Created by Andres Raigoza on 27/03/26.
+//
+
+import Testing
+
+struct Test {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
